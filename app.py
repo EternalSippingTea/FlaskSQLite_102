dacaa7fd02dbecd4a4baa2990216281fa8f94ead
EternaBliz <farhanid0105@gmail.com>
2026-01-06 08:29:52 +0000

    fix app.py

--- app.py
+++ app.py
@@ -1,46 +1,46 @@
-import sqlite3 as sql3
+import sqlite3
 from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 DB_NAME = "books.db"
 
-def connect_db():
-    conn = sql3.connect(DB_NAME)
-    conn.row_factory = sql3.Row
+def connectdb():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
     return conn
 
 def init_db():
-    conn = connect_db()
+    conn = connectdb()
     conn.execute('''CREATE TABLE IF NOT EXISTS books (
-                        id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        judul VARCHAR(100) NOT NULL,
-                        penulis VARCHAR(100) NOT NULL
-                    );''')
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    judul VARCHAR(100) NOT NULL,
+                    penulis VARCHAR(100) NOT NULL
+                );''')
     conn.commit()
     conn.close()
 
 @app.route('/')
 def index():
-    conn = connect_db()
+    conn = connectdb()
     books = conn.execute("SELECT * FROM books").fetchall()
     conn.close()
     return render_template('index.html', books=books)
 
-@app.route('/add', methods=['GET','POST'])
+@app.route('/add', methods=['GET', 'POST'])
 def add():
     if request.method == 'POST':
         judul = request.form['judul']
         penulis = request.form['penulis']
-        conn = connect_db()
+        conn = connectdb()
         conn.execute("INSERT INTO books (judul, penulis) VALUES (?, ?)", (judul, penulis))
         conn.commit()
         conn.close()
         return redirect(url_for('index'))
     return render_template('add.html')
 
-@app.route('/edit/<int:id>', methods=['GET','POST'])
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
-    conn = connect_db()
+    conn = connectdb()
     book = conn.execute("SELECT * FROM books WHERE id = ?", (id,)).fetchone()
     if not book:
         return "Buku tidak ditemukan", 404
@@ -54,11 +54,11 @@ def edit(id):
         return redirect(url_for('index'))
     
     conn.close()
-    return render_template('edit.html', book=book)  
+    return render_template('edit.html', book=book)
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    conn = connect_db()
+    conn = connectdb()
     conn.execute("DELETE FROM books WHERE id = ?", (id,))
     conn.commit()
     conn.close()
@@ -67,4 +67,3 @@ def delete(id):
 if __name__ == '__main__':
     init_db()
     app.run(host='0.0.0.0', port=6001, debug=True)
-    
